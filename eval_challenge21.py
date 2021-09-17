050933e78230f5029737db1765b28b08a3d7a128
flytocc <flytocc@outlook.com>
2021-09-17 14:00:26 +0800

    update

--- eval_challenge21.py
+++ eval_challenge21.py
@@ -1,6 +1,6 @@
-import itertools
 import json
 import os
+import time
 
 from lvis import LVIS, LVISEval, LVISResults
 
@@ -12,46 +12,33 @@ RESULT_DIR = "../Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-T-FPN-GCE_fed_
 RESULT_PATH = os.path.join(RESULT_DIR, 'merged_segm_10k_dets_per_cat.json')
 
 print("create lvis_gt")
+tic = time.time()
 lvis_gt = LVIS(ANNOTATION_PATH, precompute_boundary=True)
-
-# bbox_list = []
-# for root, dirs, files in os.walk(RESULT_DIR):
-#     for name in files:
-#         if name.startswith('bbox_'):
-#             fn = os.path.join(root, name)
-#             with open(fn, 'r') as f:
-#                 sub_res = json.load(f)
-#             bbox_list.append(sub_res)
-# bbox = list(itertools.chain.from_iterable(bbox_list))
-
-# bbox_dt = LVISResults(lvis_gt, bbox)
-# bbox_eval = LVISEval(lvis_gt, bbox_dt, iou_type='bbox')
-# bbox_eval.run()
-# bbox_eval.print_results()
-
-# del bbox_eval, bbox_dt, bbox
+print('create lvis_gt (t={:0.2f}s)'.format(time.time() - tic))
 
 print("load segm res")
+tic = time.time()
 with open(RESULT_PATH, 'r') as f:
     segm = json.load(f)
-
-# segm_dt = LVISResults(lvis_gt, segm)
-# segm_eval = LVISEval(lvis_gt, segm_dt, iou_type='segm')
-# segm_eval.run()
-# segm_eval.print_results()
-
-# del segm_eval, segm_dt
+print('load segm res (t={:0.2f}s)'.format(time.time() - tic))
 
 print("create boundary_dt")
+tic = time.time()
 boundary_dt = LVISResults(
     ANNOTATION_PATH, segm,
     max_dets_per_cat=10000,
     max_dets_per_im=-1,
     precompute_boundary=True)
+print('create boundary_dt (t={:0.2f}s)'.format(time.time() - tic))
 
 print("create boundary_eval")
+tic = time.time()
 boundary_eval = LVISEval(lvis_gt, boundary_dt, iou_type='boundary', mode='challenge2021')
+print('create boundary_eval (t={:0.2f}s)'.format(time.time() - tic))
 
 print("run boundary_eval")
+tic = time.time()
 boundary_eval.run()
+print('run boundary_eval (t={:0.2f}s)'.format(time.time() - tic))
+
 boundary_eval.print_results()
