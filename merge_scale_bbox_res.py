116e4729ae8bf93165da6840a4729afb4a9c9318
flytocc <lenny.n@outlook.com>
2021-09-25 00:48:25 +0800

    update

---
+++ merge_scale_bbox_res.py
@@ -0,0 +1,79 @@
+import argparse
+import itertools
+import os
+import json
+import warnings
+from collections import defaultdict
+
+import tqdm
+
+
+def limit_dets_per_cat(anns, max_dets):
+    by_cat = defaultdict(list)
+    for ann in anns:
+        by_cat[ann["category_id"]].append(ann)
+    results = []
+    fewer_dets_cats = set()
+    for cat, cat_anns in tqdm.tqdm(by_cat.items()):
+        if len(cat_anns) < max_dets:
+            fewer_dets_cats.add(cat)
+        elif len(cat_anns) > max_dets:
+            cat_anns = sorted(cat_anns, key=lambda x: x["score"], reverse=True)[:max_dets]
+        results.extend(cat_anns)
+    if fewer_dets_cats:
+        warnings.warn(
+            f"{len(fewer_dets_cats)} categories had less than {max_dets} "
+            f"detections!\n"
+            f"Outputting {max_dets} detections for each category will improve AP "
+            f"further."
+        )
+    return results
+
+
+def main(args):
+    SAVE_PATH = os.path.join(args.res_dir, f'merged_bbox_top{args.limit}_dets_per_cat_{args.scale}.json')
+
+    print("load bbox res")
+    bbox_fn_list = []
+    for root, dirs, files in os.walk(args.res_dir, followlinks=True):
+        scale = root.split('/')[-1]
+        if args.scale != scale:
+            continue
+        for name in files:
+            if name.startswith(args.prefix):
+                fn = os.path.join(root, name)
+                bbox_fn_list.append(fn)
+
+    bbox_list = []
+    for idx, fn in enumerate(bbox_fn_list, 1):
+        print(f"[{idx}/{len(bbox_fn_list)}] loading {fn}")
+        with open(fn, 'r') as f:
+            sub_res = json.load(f)
+        bbox_list.append(sub_res)
+    bbox = itertools.chain.from_iterable(bbox_list)
+
+    print("run limit_dets_per_cat")
+    bbox_13k_dets_per_cat = limit_dets_per_cat(bbox, args.limit)
+
+    print("set ann id")
+    ann_id = 1
+    for ann in bbox_13k_dets_per_cat:
+        ann['id'] = ann_id
+        ann_id = ann_id + 1
+
+    if len(bbox_13k_dets_per_cat) > 0:
+        print(f"save to {SAVE_PATH}")
+        with open(SAVE_PATH, 'w') as f:
+            json.dump(bbox_13k_dets_per_cat, f)
+
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="merge_scale_bbox_res")
+    parser.add_argument('--res_dir', type=str, default='/home/nieyang/Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-L-FPN-GCE-64ROI-MASKNORM_fed_rfs_0.5x_ms-pretrained@64ROI/res')
+    parser.add_argument('--scale', type=str, default='800')
+    parser.add_argument('--prefix', type=str, default='bbox_')
+    parser.add_argument('--limit', type=int, default=13000)
+    args = parser.parse_args()
+
+    main(args)
--- test_challenge21.py
+++ test_challenge21.py
@@ -1,10 +1,27 @@
+import argparse
 from lvis import LVISEval
+import time
 
-if __name__ == "__main__":
-    # result and val files for 100 randomly sampled images.
-    ANNOTATION_PATH = "./data/lvis_val_100.json"
-    RESULT_PATH = "./data/lvis_results_100.json"
 
-    lvis_eval = LVISEval(ANNOTATION_PATH, RESULT_PATH, mode="challenge2021")
+def main(args):
+    start_time = time.time()
+    if args.mode in ('bbox', 'segm'):
+        lvis_eval = LVISEval(args.ann, args.res, iou_type=args.mode)
+    elif args.mode == 'boundary':
+        lvis_eval = LVISEval(args.ann, args.res, mode='challenge2021')
+    else:
+        raise ValueError(f"Unsupported mode: {args.mode}")
     lvis_eval.run()
     lvis_eval.print_results()
+    print(f"Total time: {time.time() - start_time}")
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="merge_scale_bbox_res")
+    parser.add_argument('--res', type=str, default='/home/nieyang/Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-L-FPN-GCE-64ROI-MASKNORM_fed_rfs_0.5x_ms-pretrained@64ROI/res/merged_bbox_top13000_dets_per_cat_800.json')
+    parser.add_argument('--ann', type=str, default='/home/user/Database/MSCOCO2017/annotations/lvis/lvis_v1_val.json')
+    parser.add_argument('--mode', choices=('bbox', 'segm', 'boundary'), type=str, default='bbox')
+    args = parser.parse_args()
+
+    main(args)
