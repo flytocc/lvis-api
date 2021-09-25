85fbfddbfde98224795e2bfcfaabc3d333a0a2d3
flytocc <lenny.n@outlook.com>
2021-09-25 19:44:51 +0800

    update

--- merge_wbf_bbox_res.sh
+++ merge_wbf_bbox_res.sh
@@ -3,6 +3,5 @@
 RES_DIR="/home/nieyang/Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-L-FPN-GCE-64ROI-MASKNORM_fed_rfs_0.5x_ms-pretrained@64ROI/res"
 
 PYTHON=${PYTHON:-"python"}
-for SACLE in ${SCALES[@]};do
-    $PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale res --prefix wbf_bbox_ --limit 15000
-done
+
+$PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale res --prefix wbf_bbox_ --limit 15000
