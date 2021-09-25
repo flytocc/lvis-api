02940b859864598d7c565578b822318f9360687c
flytocc <lenny.n@outlook.com>
2021-09-26 00:50:03 +0800

    update

--- merge_comb_segm_res.sh
+++ merge_comb_segm_res.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 RES_DIR="/home/nieyang/Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-L-FPN-GCE-64ROI-MASKNORM_fed_rfs_0.5x_ms-pretrained@64ROI/res"
-SCALES=$1  # "600 700 800 900 1000 1100 1200"
 
 PYTHON=${PYTHON:-"python"}
-for SACLE in ${SCALES[@]};do
-    $PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale $SACLE
-done
+
+$PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale res --prefix comb_segm_
