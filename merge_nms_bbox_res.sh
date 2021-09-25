38b1d39fd66ee630a0d47de2505f1625c6523ce0
flytocc <flytocc@outlook.com>
2021-09-26 01:46:43 +0800

    update

---
+++ merge_nms_bbox_res.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+RES_DIR="/home/nieyang/Pet-dev/ckpts/cnn/LVIS/swin/centernet2-mask_SWIN-L-FPN-GCE-64ROI-MASKNORM_fed_rfs_0.5x_ms-pretrained@64ROI/res"
+
+PYTHON=${PYTHON:-"python"}
+
+$PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale res --prefix nms_bbox_ --limit 12000
