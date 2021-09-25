00bff329c30b0ba1e9b7bd594e76e557db76ba0e
flytocc <lenny.n@outlook.com>
2021-09-25 14:27:14 +0800

    update

--- merge_scale_bbox_res.sh
+++ merge_scale_bbox_res.sh
@@ -5,5 +5,5 @@ SCALES=$1  # "500 600 700 800 900 1000 1100 1200 1300"
 
 PYTHON=${PYTHON:-"python"}
 for SACLE in ${SCALES[@]};do
-    $PYTHON merge_scale_bbox_res.py --res_dir $RES_DIR --scale $SACLE
+    $PYTHON merge_bbox_res.py --res_dir $RES_DIR --scale $SACLE
 done
